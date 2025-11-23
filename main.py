df14a658e5b3574f2515c70fe015368516d141ac
Henrique Petiz <heishenrique@gmail.com>
2025-11-23 13:06:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,4 +21,32 @@ except Exception:
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+
+
+
+### Printa as colunas para melhor organização do código
+
+df = pd.read_csv('data/steam_games.csv')
+print(df.columns)
+
+### Converte a coluna de Data de Lançamento para o formato datetime
+df['Release date'] = pd.to_datetime(df['Release date'], errors='coerce')
+
+###melhora comunicação com coluna de Data de Lançamento
+df.rename(columns={'Release date': 'release_date'}, inplace=True)
+
+### Separa os gêneros em uma lista criando uma linha para cada gênero
+df['Genres'] = df['Genres'].str.split(';').str.strip()
+df_exploded_genres = df.explode('Genres')
+
+### Converte a coluna de Pontuação Metacritic para numérico
+df['Metacritic score'] = pd.to_numeric(df['Metacritic score'], errors='coerce')
+
+# Removendo linhas onde a pontuação Metacritic é NaN, pois é o foco da P1
+df.dropna(subset=['Metacritic score'], inplace=True)
+
 
