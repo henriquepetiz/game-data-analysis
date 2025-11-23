ed356c7249af0644b71561d9c43f658ca4924791
Henrique Petiz <heishenrique@gmail.com>
2025-11-23 12:48:23 -0300

    Update main.py
    extract data file

---
+++ main.py
@@ -0,0 +1,17 @@
+import zipfile; z = zipfile.ZipFile('steam_games.zip'); 
+
+print('\n'.join(z.namelist()[:10]))
+
+import zipfile
+import os
+
+if not os.path.exists('data/'):
+    os.makedirs('data/')
+
+try:
+    with zipfile.ZipFile('steam_games.zip', 'r') as zip_ref:
+        zip_ref.extractall('data/')
+except FileNotFoundError:
+    pass
+except Exception:
+    pass
